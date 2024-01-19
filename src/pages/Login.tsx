aa115e4c85a05891b70f2a0478ee0a1c8828046d
shahadat52 <shahadathossain.sh255@gmail.com>
2024-01-19 23:54:10 +0600

    login userData and token set in redux state

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
     const dispatch = useAppDispatch()
@@ -21,8 +22,11 @@ const Login = () => {
             password: data.password
         }
         const res = await login(userInfo).unwrap()
-        console.log({ res });
-        
+        const token = res.data.accessToken
+        const user = verifyToken(token)
+        dispatch(setUser({ user, token }))
+
+
     }
     return (
         <div>
